0496c74d8678fb70bcc15524a45150ebef7bb836
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-21 15:39:42 -0300

    getLinks in spreadsheet

--- pages/api/getLinks.js
+++ pages/api/getLinks.js
@@ -8,7 +8,19 @@ const getLinks = async (req, res) => {
         private_key: process.env.SPREADSHEET_PRIVATE_KEY
     })
     await doc.loadInfo()
-    res.send(doc.title)
+
+    const sheet = doc.sheetsById['0']
+    const rows = await sheet.getRows()
+    const linkTree = rows.map(row => ({
+        title: row.Title,
+        link: row.Link
+    }))
+
+    res.send(linkTree)
+}
+
+const postAccess = async () => {
+
 }
 
 export default getLinks
